35ec35375108142b9618dfeaad1d86c675a11245
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2020-02-20 11:19:53 -0600

    Análisi léxico

--- src/File.kt
+++ src/File.kt
@@ -2,6 +2,19 @@ import java.io.FileReader
 
 class File (src : String) {
     private val fileReader = FileReader(src)
+    var lastReadChar : Char = 0.toChar()
 
-    fun lastChar() = fileReader.read().toChar()
+    init {
+        readChar()
+    }
+
+    companion object {
+        const val EOF = (-1).toChar()
+    }
+
+    fun readChar() : Char{
+        val char = fileReader.read().toChar()
+        lastReadChar = char
+        return char
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,5 @@
+import java.nio.file.Paths
+
 /*
 * Ramiro Estrada García
 * 2015190034
@@ -5,12 +7,80 @@
 * 2015190005
 */
 
+val file = File("${Paths.get("").toAbsolutePath()}/src/res/test.txt")
+
 fun main(){
-    val file = File("C:/test.txt")
-    println(file.lastChar())
-    println(file.lastChar())
-    println(file.lastChar())
-    println(file.lastChar())
-    println(file.lastChar())
-    println(file.lastChar())
+    println(metodo())
+    println(metodo())
+    println(metodo())
+    println(metodo())
 }
+
+fun metodo() : String{
+    val alpha = matchAlpha()
+    if(alpha.isNotEmpty()){
+        return alpha
+    }
+
+    val num = matchNumber()
+    if(num.isNotEmpty()){
+        return num
+    }
+
+    val symbol = matchSymbol()
+    if(symbol.isNotEmpty()){
+        return symbol
+    }
+
+    return ""
+}
+
+fun matchAlpha() : String{
+    var res = ""
+    while (file.lastReadChar != File.EOF){
+        if(file.lastReadChar.isLetter() || file.lastReadChar == '_'){
+            res += file.lastReadChar
+            while (file.readChar().isLetter() || file.lastReadChar == '_' || file.lastReadChar.isDigit()){
+                res += file.lastReadChar
+            }
+        }else{
+            return res
+        }
+    }
+
+    return res
+}
+
+fun matchSymbol() : String{
+    var res = ""
+
+    if(!(file.lastReadChar.isLetter() || file.lastReadChar.isDigit() || file.lastReadChar == '_')) {
+        res += file.lastReadChar
+        file.readChar()
+
+        if(((res.first() == '|' || res.first() == '=' || res.first() == '&') && res.first() == file.lastReadChar) ||
+            ((res.first() == '>' || res.first() == '<' || res.first() == '!') && file.lastReadChar == '=')) {
+            res += file.lastReadChar
+            file.readChar()
+        }
+    }
+
+
+    return res
+}
+
+fun matchNumber() : String{
+    var res = ""
+    while (file.lastReadChar != File.EOF){
+        if(file.lastReadChar.isDigit() || file.lastReadChar == '.') {
+            res += file.lastReadChar
+        }else{
+            return res
+        }
+
+        file.readChar()
+    }
+
+    return res
+}
+
